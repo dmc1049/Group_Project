a4ae99e235a87992068f09cb0bea34f15a1a0150
Don <dmc1049@wildcats.unh.edu>
2019-11-24 15:16:51 -0500

    Updated-3:15PM-11-24-2019
    Updated Player.java and GameEngineTwo.java to work with each other for story/game

--- GameEngineTwo.java
+++ GameEngineTwo.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class GameEngineTwo {
 
+	private static final String NEWLINE = System.getProperty("line.separator");//This will retrieve line separator dependent on OS.
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//
@@ -21,49 +23,176 @@ public class GameEngineTwo {
 		
 		//basic code for game
 		
-		String PlayerClass = Player.PlayerClassType();
-		String PlayerCharacter = Player.PlayerCharacterName();
+		//String PlayerClass = Player.PlayerClassType();
+		//String PlayerCharacter = Player.PlayerCharacterName();
+		Player.CheckSavedCharacter();
 		
 		String OptionChoose = "";
-		String newLine = System.getProperty("line.separator");//This will retrieve line separator dependent on OS.
 		
-		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println(String.format("Your Character is %s, the name of your character is %s", PlayerClass, PlayerCharacter));
+		Scanner scannerMain = new Scanner(System.in);
+		
+		System.out.println(String.format("Your Character is %s, the name of your character is %s", Player.CHARACTER_TYPE, Player.CHARACTER_NAME));
 		System.out.println("");
 		
 		System.out.println("Welcome to TAG, you are currently in a Town named One. Enjoy! ");
 		System.out.println("");
-		System.out.println("To play the game you need to select what you want to do from the options given to you. For this first town you can select fro mthe options below.");
-		System.out.println("");
+		System.out.println("To play the game you need to select what you want to do from the options given to you. " + NEWLINE + "For this first town you can select from the options below.");
+		System.out.println("--------------");
+		CurrentTown();
+			 
+		//scannerMain.close();
+	}
+
+	private static void ncpWitch() {
+		// TODO Auto-generated method stub
+		String ncpOption = "";
+		Scanner scannerNCPWitch = new Scanner(System.in);
+		System.out.println(NEWLINE + "Hello I am the local Witch, What can I do for you?" + NEWLINE + "H) Get healed" + NEWLINE + "T) Talk to Witch about this town" + NEWLINE + "A) Ask for a Date" + NEWLINE + "X) Leave Witch" + NEWLINE +":");
+		ncpOption = scannerNCPWitch.next();
+		ncpOption = ncpOption.toUpperCase();
+		//scannerNCPWitch.close();
+		switch(ncpOption){
+			case "H":
+				//Heal player
+				System.out.println("The Witch heals you, now go away!");
+				//break;
+				ncpWitch();
+			case "T":
+				//talk to Witch
+				System.out.println("The Witch ignores you.");
+				//break;
+				ncpWitch();
+			case "A":
+				//ask for date
+				System.out.println("The Witch turns you into a frog...");
+				//break;
+				ncpWitch();
+			case "X":
+				//leave Witch
+				System.out.println("You thank the Witch and leave as fast as you can!" + NEWLINE);
+				//return to town
+				CurrentTown();
+				//break;
+			//default:
+				//default option
 		
-		System.out.print("A) Go to Next Town" + newLine + "B) go to Black Smith" + newLine + "C) Go to Witch" + newLine + "X) Exit Game" +newLine + "Choose Wisely: " );
-		OptionChoose = scanner.next();
-		//scanner.close();
+		}
+		
+	}
+
+	private static void ncpBlackSmith() {
+		// TODO Auto-generated method stub
+		String ncpOption = "";
+		Scanner scannerNCPWitch = new Scanner(System.in);
+		System.out.println(NEWLINE + "Hello I am the local BlackSmith, What can I do for you?" + NEWLINE + "W) Get Weapons fixed" + NEWLINE + "T) Talk to Blacksmith about this town" + NEWLINE + "Q) Ask about the Quest" + NEWLINE + "X) Leave Blacksmith" + NEWLINE +":");
+		ncpOption = scannerNCPWitch.next();
+		ncpOption = ncpOption.toUpperCase();
+		//scannerNCPWitch.close();
+		switch(ncpOption){
+			case "W":
+				//Heal player
+				System.out.println("The Blacksmith repairs your weapons.");
+				//break;
+				ncpBlackSmith();
+			case "T":
+				//talk to Witch
+				System.out.println("The Blacksmith tells you that the Town is great, but you should explore the outerlands.");
+				//break;
+				ncpBlackSmith();
+			case "Q":
+				//ask for date
+				System.out.println("The tells you about how he needs something from XYZ and asks you to ...");
+				//break;
+				ncpBlackSmith();
+			case "X":
+				//leave Witch
+				System.out.println("You thank the Blacksmith and leave back to Town One." + NEWLINE);
+				//return to town
+				CurrentTown();
+				//break;
+			//default:
+				//default option
+		
+		}
+	}
+
+	private static void TravelNextTown() {
+		// TODO Auto-generated method stub
+		String OptionChoose = "";
+		Scanner scannerNextTown = new Scanner(System.in);
+		System.out.println("");
+		System.out.println("You are in Town One...Where do you want to go?" + NEWLINE);
+		System.out.print("E) Go to Next Town to East" + NEWLINE + "W) Go to Next Town to West" + NEWLINE + "N) Go to Next Town to North" + NEWLINE + "S) Go to Next Town to South" + NEWLINE + "X) Exit Back to Current Town" +NEWLINE + "Choose Wisely: " );
+		OptionChoose = scannerNextTown.next();
+		OptionChoose = OptionChoose.toUpperCase();
+		//scannerCurrentTown.close();
+		switch(OptionChoose) {
+			case "E":
+				//Go to Town to East
+				System.out.println("You will travel to the next town, over hill and through Dale, Don't get killed");
+				TravelNextTown();
+				//break;
+			case "W":
+				//Go to Town to West
+				System.out.println("You will travel to the next town, over hill and through Dale, Don't get killed");
+				TravelNextTown();
+				//break;
+			case "N":
+				//Go to Town to North
+				System.out.println("You will travel to the next town, over hill and through Dale, Don't get killed" + NEWLINE);
+				TravelNextTown();
+				//break;
+			case "S":
+				//Go to Town to South
+				System.out.println("You will travel to the next town, over hill and through Dale, Don't get killed" + NEWLINE);
+				TravelNextTown();
+				//break;
+			case "X":
+				//Exit Game
+				System.out.println("Returning to Current Town");
+				CurrentTown();
+			//default:
+				//Some code here as default option
+				
+		}
+	}
+
+	private static void CurrentTown() {
+		//TODO Auto-generated method stub
+		String OptionChoose = "";
+		Scanner scannerCurrentTown = new Scanner(System.in);		
+		System.out.println("");
+		System.out.println("You are in Town One...What do you want to do?" + NEWLINE);
+		System.out.print("A) Go to Next Town" + NEWLINE + "B) go to Black Smith" + NEWLINE + "C) Go to Witch" + NEWLINE + "X) Exit Game" +NEWLINE + "Choose Wisely: " );
+		OptionChoose = scannerCurrentTown.next();
+		OptionChoose = OptionChoose.toUpperCase();
+		//scannerCurrentTown.close();
 		switch(OptionChoose) {
 			case "A":
 				//Go to next Town
-				System.out.println("You will travel to the next town, over hill and through Dale, Don't get killed");
-				break;
+				System.out.println("You will travel to the next Town, over hill and through Dale, Don't get killed");
+				TravelNextTown();
+				//break;
 			case "B":
 				//Go to Black Smith
-				System.out.println("Bla BLa");
-				break;
+				System.out.println("The Blacksmith may have a quest for you");
+				ncpBlackSmith();
+				//break;
 			case "C":
 				//Go to Witch
-				System.out.println("Going to the Witch, don't talk long to her, she will turn you into a Frog");
-				break;
+				System.out.println("Going to the Witch, don't talk to long her, she will turn you into a Frog." + NEWLINE);
+				ncpWitch();
+				//break;
 			case "X":
 				//Exit Game
 				System.out.println("You are Exiting the game");
 				System.exit(0);
-			default:
+			//default:
 				//Some code here as default option
 				
 		}
-		
-		scanner.close();
-
 	}
+	
 
 }
--- Player.java
+++ Player.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 
 public class Player {
 
-	private static String CHARACTER_TYPE = null;
-	private static String CHARACTER_NAME = null;
+	public static String CHARACTER_TYPE = null;
+	public static String CHARACTER_NAME = null;
+	private static String FILE_NAME = "GameChar.txt";
+	private static String FILE_PATH = "";
 	
 		//is there a saved character or does the user need to make one
 			//User has saved game character
@@ -24,20 +34,129 @@ public class Player {
 		//section for weapons
 	
 		//section for cash
-	public static SaveCharacterPlayer() {
+	public static void SaveCharacterPlayer() {
 		//Save the player character when exiting the game
-		
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(FILE_NAME, "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		writer.println(CHARACTER_TYPE);
+		writer.println(CHARACTER_NAME);
+		writer.close();
 	}
 	
-	public static CheckSavedCharacter() {
+	public static void CheckSavedCharacter() {
 		//look for saved character file, if none create character, 
 		//if present ask if player wants to use this character or create new one
+		Scanner scannerCheckCharacter = new Scanner(System.in);
+		Scanner scannerDeleteCharacter = new Scanner(System.in);
+		String UseChar = "";
+		String DeleteChar = "";
+		File PlayerCharFile = new File("GameChar.txt");
+		boolean exists = PlayerCharFile.exists();
+		if (exists) {
+			System.out.print("Do you want to use saved character: Y for Yes, N for No: ");
+			UseChar = scannerCheckCharacter.next();
+			UseChar = UseChar.toUpperCase();
+			if (UseChar.equals("Y")) {
+				LoadPlayerChar();
+			}
+			if (UseChar.equals("N")) {
+				//Ask if they want to delete their current Character
+				//only one character is allowed at this moment
+				System.out.print("Do you want to Delete your current Character and create a new one? Y for Yes, N for No: ");
+				DeleteChar = scannerDeleteCharacter.next();
+				DeleteChar = DeleteChar.toUpperCase();
+				if (DeleteChar.equals("Y")) {
+					//delete char file and create new one
+					DeleteCharacterPlayer();
+					CreateCharacterPlayer();
+				}
+				else {
+					System.out.println("We will load the current Character you have saved.");
+					LoadPlayerChar();
+				}
+			}
+		}
+		else
+		{
+			CreateCharacterPlayer();
+		}
 		
 		
 	}
-	public static CreateCharacterPlayer() {
-		//Create the Character for the Player and save
+
+	private static void DeleteCharacterPlayer() {
+		// TODO Auto-generated method stub
+		// Delete the Saved Char file
+		File file = new File(FILE_NAME);
+		if(file.delete()) {
+			System.out.println("Character File Deleted");
+		}
+		else {
+			System.out.println("Failed to delete Character file");
+			System.out.println("But overwrite is working??????")
+		}
 		
+	}
+
+	private static void LoadPlayerChar() {
+		// TODO Auto-generated method stub
+		// Load Char file
+		File PlayerCharFile = new File("GameChar.txt");
+		boolean exists = PlayerCharFile.exists();
+		if(PlayerCharFile.exists() && PlayerCharFile.isFile()) {
+			//read file and populate constants of Type and Name
+			try {
+				File file = new File(FILE_NAME);
+				Scanner myReader = new Scanner(file);
+				int i = 1;
+				while (myReader.hasNextLine()) {
+					String data = myReader.nextLine();
+					if(i == 1) {
+						CHARACTER_TYPE = data;
+						i+=i;
+					}
+					else {
+						CHARACTER_NAME = data;
+					}
+				}
+			}
+			//myReader.close();
+			catch(FileNotFoundException e) {
+				System.out.println("File Not Found");
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("File not found, exists or is a file error");
+		}
+		
+	}
+
+	private static void CreateCharacterPlayer() {
+		//Create the Character for the Player and save
+		PlayerClassType();
+		PlayerCharacterName();
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(FILE_NAME, "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		writer.println(CHARACTER_TYPE);
+		writer.println(CHARACTER_NAME);
+		writer.close();
 		
 	}
 	
